693265b76003c97eda756db9961514c280107d31
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-09-06 12:16:29 +0200

    Calculated first parameters
    Calculated slope, intercept, RSS, RSE, TSS

--- linear_regr_from_scratch.py
+++ linear_regr_from_scratch.py
@@ -38,9 +38,28 @@ yy_median = main_df['SALARY'].median()
 # We are predicting a simple linear regression so there will be a linear relationship in the form
 # Y = Bx + Q
 # where Q is the intercept and B is the slope and are going to be the predicted parameters
+# In our case the result is SALARY = B * EXP + Q
+
+xx_err = [xx - xx_mean for xx in main_df['EXP']]
+yy_err = [yy - yy_mean for yy in main_df['SALARY']]
+
+errors = [xer * yer for xer, yer in zip(xx_err,yy_err)]
+pred_slope = sum(errors)/sum([x*x for x in xx_err])
+
+pred_intercept = yy_mean - pred_slope * xx_mean
+
+RSS_values = [(yy-pred_intercept-pred_slope*xx)**2 for xx, yy in zip(main_df['EXP'],main_df['SALARY'])] 
+
+RSS = sum(RSS_values)
 
 
+RSE = np.sqrt(RSS/(len(RSS_values)-2))
 
+TSS = sum([(yy - yy_mean)**2 for yy in main_df['SALARY']])
 
+R_squared = 1 - RSS/TSS
 
+std_dev = main_df.std()
 
+exp_stddev = std_dev[0]
+salary_stddev = std_dev[1]
