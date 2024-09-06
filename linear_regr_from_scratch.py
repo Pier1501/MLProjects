eb78a322299c9c46917eba84f99dae75da055ced
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-09-06 09:18:50 +0200

    Created main .py

---
+++ linear_regr_from_scratch.py
@@ -0,0 +1,7 @@
+# Machine Learning Project - Linear Regression from scratch in Python
+
+# This project is based on examples taken from the MIT project-based-learning open-source GitHub repository
+# https://github.com/practical-tutorials/project-based-learning
+
+
+
